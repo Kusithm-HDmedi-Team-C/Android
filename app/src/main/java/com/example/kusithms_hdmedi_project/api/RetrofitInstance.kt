fde148654d00ee600fd3797eb6df90d5b22be1f2
Ojongseok <whdtjr6889@naver.com>
2023-09-05 17:51:19 +0900

    gitignore에서 BASE_URL 노출되지 않도록 수정

---
+++ app/src/main/java/com/example/kusithms_hdmedi_project/api/RetrofitInstance.kt
@@ -0,0 +1,7 @@
+package com.example.kusithms_hdmedi_project.api
+
+import com.example.kusithms_hdmedi_project.BuildConfig
+
+object RetrofitInstance {
+    const val BASE_URL = BuildConfig.BASE_URL
+}
