f72fa89b87ef1ccc31301cd913d73bf8e6ba4d92
Ojongseok <whdtjr6889@naver.com>
2023-09-12 14:38:05 +0900

    방문내역 인증 - 이미지 첨부 제외 완료

--- app/src/main/java/com/example/kusithms_hdmedi_project/view/hospital/WriteReviewActivity.kt
+++ app/src/main/java/com/example/kusithms_hdmedi_project/view/hospital/WriteReviewActivity.kt
@@ -1,7 +1,10 @@
 package com.example.kusithms_hdmedi_project.view.hospital
 
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
 import com.example.kusithms_hdmedi_project.R
 import com.example.kusithms_hdmedi_project.base.BaseDialog
 import com.example.kusithms_hdmedi_project.databinding.ActivityHospitalBinding
@@ -16,6 +19,10 @@ class WriteReviewActivity : AppCompatActivity() {
         _binding = ActivityWriteReviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.tvUploadImage.setOnClickListener {
+            imageUploadComplete()
+        }
+
         binding.btnClose.setOnClickListener {
             BaseDialog(
                 context = this@WriteReviewActivity,
@@ -26,6 +33,15 @@ class WriteReviewActivity : AppCompatActivity() {
                 },
                 negative = {}
             ).show(supportFragmentManager, "")
+        }
+    }
+
+    private fun imageUploadComplete() {
+        binding.llImageUploadOff.visibility = View.INVISIBLE
+        binding.llImageUploadOn.visibility = View.VISIBLE
+        binding.tvWriteReview.apply {
+            isClickable = true
+            backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(this@WriteReviewActivity, R.color.main_blue))
         }
     }
 
