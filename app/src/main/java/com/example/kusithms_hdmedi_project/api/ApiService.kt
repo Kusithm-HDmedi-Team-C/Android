80154e1740ce5e6c21e038f40cf3678ca50e2fed
songseunghui <seunghuisong41@gmail.com>
2023-09-16 10:48:39 +0900

    api service 수정

--- app/src/main/java/com/example/kusithms_hdmedi_project/api/ApiService.kt
+++ app/src/main/java/com/example/kusithms_hdmedi_project/api/ApiService.kt
@@ -15,10 +15,9 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
-
 import retrofit2.http.Part
 import retrofit2.http.PartMap
-
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
